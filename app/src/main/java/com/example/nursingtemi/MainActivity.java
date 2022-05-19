f3d309554c874303f8db371eaf724cceb5306034
Nate <n.teku710@gmail.com>
2022-05-18 21:47:42 -0700

    implemented Temi SDK inside project

--- app/src/main/java/com/example/nursingtemi/MainActivity.java
+++ app/src/main/java/com/example/nursingtemi/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.nursingtemi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.robotemi.sdk.Robot;
+import com.robotemi.sdk.TtsRequest;
+import com.robotemi.sdk.listeners.OnRobotReadyListener;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 }
