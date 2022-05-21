dbb321fcd194b20bf80c922bd96cc204a9db9b85
Nate <n.teku710@gmail.com>
2022-05-20 17:27:32 -0700

    updated menu and temi sdk, buttons need implementation

--- app/src/main/java/com/example/nursingtemi/MainActivity.java
+++ app/src/main/java/com/example/nursingtemi/MainActivity.java
@@ -5,15 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.robotemi.sdk.Robot;
 import com.robotemi.sdk.TtsRequest;
 import com.robotemi.sdk.listeners.OnRobotReadyListener;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button tourButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        tourButton = findViewById(R.id.tourButton);
+        tourButton.setOnClickListener((v) -> {
+            openActivity(TourActivity.class);
+        });
+
+    }
+
+
+    public void openActivity(Class a){
+        Intent obj = new Intent(this,a);
+        startActivity(obj);
     }
 }
