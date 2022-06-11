57f52bb40ef85d094134142568ccd88b67cb80c5
Nate <n.teku710@gmail.com>
2022-06-11 14:06:16 -0700

    Created SurveyActivity

--- app/src/main/java/com/example/nursingtemi/MainActivity.java
+++ app/src/main/java/com/example/nursingtemi/MainActivity.java
@@ -15,6 +15,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     private Button tourButton;
     private Button deliveryButton;
+    private Button surveyButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +32,13 @@ public class MainActivity extends AppCompatActivity {
             openActivity(DeliveryActivity.class);
         });
 
+        surveyButton = findViewById(R.id.surveyButton);
+        surveyButton.setOnClickListener((v)->
+                openActivity(SurveyActivity.class));
+
     }
+
+
 
 
     public void openActivity(Class a){
