f89d81f1f4cc05ddee86bda581288f55bdc4bac5
Admin <ishikapatel836@gmail.com>
2022-06-30 11:43:53 -0700

    New "Information" Activity Created

--- app/src/main/java/com/example/nursingtemi/InfoActivity.java
+++ app/src/main/java/com/example/nursingtemi/InfoActivity.java
@@ -2,7 +2,10 @@ package com.example.nursingtemi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+    }
+
+    public void nursingWebsite(View view) {
+        Intent websiteIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.nursing.arizona.edu/welcome"));
+        startActivity(websiteIntent);
     }
 }
--- app/src/main/java/com/example/nursingtemi/MainActivity.java
+++ app/src/main/java/com/example/nursingtemi/MainActivity.java
@@ -12,6 +12,7 @@ import android.bluetooth.BluetoothA2dp;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
