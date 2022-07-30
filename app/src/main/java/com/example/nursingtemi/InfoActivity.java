30b889eaa0da990c3984fb82cf2559f24240d2eb
Nate <n.teku710@gmail.com>
2022-07-30 15:14:52 -0700

    Working on Info Activity

--- app/src/main/java/com/example/nursingtemi/InfoActivity.java
+++ app/src/main/java/com/example/nursingtemi/InfoActivity.java
@@ -1,24 +1,37 @@
 package com.example.nursingtemi;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InfoActivity extends AppCompatActivity {
 
-    private WebView webView;
 
+    private Button urlButton;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_info);
 
-        webView = findViewById(R.id.webView);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.nursing.arizona.edu/simulated-learning");
+
+        urlButton = findViewById(R.id.urlButton);
+
+        urlButton.setOnClickListener((v)->{
+            webView = (WebView) findViewById(R.id.webviewButton);
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.loadUrl("https://www.nursing.arizona.edu/simulated-learning");
+            //startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.nursing.arizona.edu/simulated-learning")));
+        });
+
+
 
     }
 }
