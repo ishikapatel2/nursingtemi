cfb72d97105dcfa67606f2a1c7446e7448594c5c
Admin <ishikapatel836@gmail.com>
2022-08-03 14:05:50 -0700

    Website Link

--- app/src/main/java/com/example/nursingtemi/InfoActivity.java
+++ app/src/main/java/com/example/nursingtemi/InfoActivity.java
@@ -1,5 +1,7 @@
 package com.example.nursingtemi;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
@@ -11,21 +13,20 @@ public class InfoActivity extends AppCompatActivity {
 
 
     private Button urlButton;
-    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_info);
-        webView = findViewById(R.id.webviewButton);
-        webView.setVisibility(View.INVISIBLE);
-        urlButton = findViewById(R.id.urlButton);
-
-        urlButton.setOnClickListener((v)->{
-            webView.setVisibility(View.VISIBLE);
-            webView.getSettings().setJavaScriptEnabled(true);
-            webView.loadUrl("https://www.nursing.arizona.edu/simulated-learning");
+        urlButton = (Button) findViewById(R.id.urlButton);
+        urlButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myLink = new Intent(Intent.ACTION_VIEW);
+                myLink.setData(Uri.parse("https://www.nursing.arizona.edu/simulated-learning"));
+                startActivity(myLink);
+            }
         });
     }
 }
