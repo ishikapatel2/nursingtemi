9e01435458ce711e1f1b9a0c2002e549b237d9a5
Admin <ishikapatel836@gmail.com>
2022-08-05 16:03:13 -0700

    Website Link

--- app/src/main/java/com/example/nursingtemi/InfoActivity.java
+++ app/src/main/java/com/example/nursingtemi/InfoActivity.java
@@ -20,6 +20,9 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_info);
+        webView = (WebView) findViewById(R.id.webView1);
+        webView.setVisibility(View.GONE);
+
     }
     public void open(View view) {
         webView = (WebView) findViewById(R.id.webView1);
